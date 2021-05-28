a9c3e41a6386bc25efab8cd15def294d176d87eb
Mahelajay <57103078+Mahelajayasinghe@users.noreply.github.com>
2021-05-28 18:55:57 +0530

    react

---
+++ src/comps/Mousemove.js
@@ -0,0 +1,26 @@
+import React ,{useState,useEffect} from 'react'
+
+function Mousemove() {
+    const [x, setx] = useState(0)
+    const [y, sety] = useState(0)
+
+    useEffect(() => {
+        console.log('Use effect called')
+        window.addEventListener('mousemove',mouseposition)
+    },[])
+
+    const mouseposition = (e)=>{
+        console.log('mouse position called')
+        setx(e.clientX)
+        sety(e.clientY)
+    }
+
+    return (
+        <div>
+            <h4>X position - {x}</h4>
+            <h4>Y position - {y}</h4>
+        </div>
+    )
+}
+
+export default Mousemove
